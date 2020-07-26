540a865e15df58eac6d148fb1688a7f89fc3efda
Hritik Sapra <hritiksapra@gmail.com>
2020-07-26 21:45:54 +0530

    Revert "CoMOTO"

--- co-manipulation/comanipulationpy/src/spider_plot_data.py
+++ co-manipulation/comanipulationpy/src/spider_plot_data.py
@@ -1,6 +1,5 @@
 import os
 import csv
-import matlab.engine
 
 def test_to_format(test_number, lines):
     """
@@ -9,36 +8,30 @@ def test_to_format(test_number, lines):
     test_number: line number in csv minus 2 (ignore column header and start index at 0)
     lines: all of the csv rows formatted as a list of strings
     """
-    eng = matlab.engine.start_matlab()
-    eng.cd('spiderPlot', nargout=0)
     l = lines[test_number]
-    matlab_inputs = []
+    test = l[0]
+    print("Test:", test)
     for i, t in zip(range(5), [0,2,3,4,1]):
         avgs = []
         sds = []
-        min_sd = 0.0001
         for j in range(4):
             data = l[(t * 4) + (j + 1)]
             avg_sd = data.split(' +/- ')
             avg = float('%.4f'%(float(avg_sd[0]))) * 100
             sd = float('%.4f'%(float(avg_sd[1]))) * 100
             if j == 3:
-                avg /= -100
+                avg /= 100
+                avg *= -1
                 sd /= 100
-            if sd < min_sd:
-                sd = min_sd
             avgs.append(avg)
             sds.append(sd)
-        matlab_inputs.append([e for e in avgs])
-        matlab_inputs.append([e for e in sds])
-        # print("D" + str(i + 1) + " = [" + ' '.join([str(elem) for elem in avgs]) + "];")
-        # print("E" + str(i + 1) + " = [" + ' '.join([str(elem)+"," for elem in sds[:-1]]) + str(sds[-1]) + "];")
+        print("D" + str(i + 1) + " = [" + ' '.join([str(elem) for elem in avgs]) + "];")
+        print("E" + str(i + 1) + " = [" + ' '.join([str(elem)+"," for elem in sds[:-1]]) + str(sds[-1]) + "];")
         avgs = []
         sds = []
-    eng.spiderPlot(matlab.double(matlab_inputs), nargout=0)
 
 
-def make_spider_plot():
+def to_spider_plot_format():
     """
     Takes the data from ExperimentResults and outputs the format of the data to create the graphs.
     Will be able to copy and paste the output to radar_test.m for graph creation.
@@ -49,4 +42,6 @@ def make_spider_plot():
     for line in file:
         lines.append(line)
     lines = lines[1:]
-    test_to_format(-1, lines)
+    test_to_format(1, lines)
+
+to_spider_plot_format()
